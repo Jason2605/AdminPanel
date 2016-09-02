103c0b22fec63dd678ad6e16a10c6af3d2ebe6cb
Jason2605 <Jason2605@users.noreply.github.com>
2016-09-02 23:47:33 +0100

    Add files via upload

---
+++ chatRoom/publish.php
@@ -0,0 +1,38 @@
+<?php
+
+session_id ("user");
+session_start();
+
+require 'vendor/autoload.php';
+
+// Configure the data store
+
+$dir = __DIR__.'/data';
+
+$config = new \JamesMoss\Flywheel\Config($dir, array(
+    'formatter' => new \JamesMoss\Flywheel\Formatter\JSON,
+));
+
+$repo = new \JamesMoss\Flywheel\Repository('shouts', $config);
+    
+// Store the posted shout data to the data store
+
+if(isset($_POST["comment"])) {
+    
+    $name = htmlspecialchars($_COOKIE['cookie']);
+    $name = str_replace(array("\n", "\r"), '', $name);
+
+    $comment = htmlspecialchars($_POST["comment"]);
+    $comment = str_replace(array("\n", "\r"), '', $comment);
+    
+    // Storing a new shout
+
+    $shout = new \JamesMoss\Flywheel\Document(array(
+        'text' => $comment,
+        'name' => $name,
+        'createdAt' => time()
+    ));
+    
+    $repo->store($shout);
+    
+}
