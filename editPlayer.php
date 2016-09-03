73eb228c6482ad3bc9f2e3f50b9e242e5fe04b4e
Jason_000 <jasonhall96686@yahoo.com>
2016-09-03 15:13:26 +0100

    Remove dead stuff

--- editPlayer.php
+++ editPlayer.php
@@ -17,7 +17,7 @@ masterconnect();
 
 $sql = "SELECT * FROM `players` WHERE uid = '$uidPlayer'";
 $result = mysqli_query($dbcon, $sql);
-$player = $result->fetch_object();	
+$player = $result->fetch_object();
 
 $username = $player->name;
 
@@ -36,10 +36,10 @@ function stripArray($input, $type)
     return $array;
 }
 
-function before($this, $inthat) 
-{ 
-      return substr($inthat, 0, strpos($inthat, $this)); 
-} 
+function before($this, $inthat)
+{
+      return substr($inthat, 0, strpos($inthat, $this));
+}
 ?>
 
 
@@ -78,7 +78,7 @@ function before($this, $inthat)
   </head>
 
   <body>
-  
+
 <?php
 include('header/header.php');
 ?>
@@ -87,7 +87,7 @@ include('header/header.php');
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 style = "margin-top: 70px">Edit Player</h1>
 		  <p class="page-header">Edit player menu of the panel, allows you to change values in more depth.</p>
-		  
+
 		  <form action='editPlayer.php' method='post'>
 		  <div class="btn-group" role="group" aria-label="...">
 		  <input class = 'btn btn-primary btn-outline' type='submit' name='remove' value='Reset Civ Licenses'>
@@ -95,38 +95,14 @@ include('header/header.php');
 		  <div class="btn-group" role="group" aria-label="...">
 		  <input class = 'btn btn-primary btn-outline' type='submit' name='give' value='Give All Civ Licenses'>
 		  </div></form> <br>
-		  
-<!--
-          <div class="row placeholders">
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Label</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Label</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Label</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Label</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-          </div>
--->
+
 <div class='panel panel-info'>
   <div class='panel-heading'>
     <h3 class='panel-title'>Player Info</h3>
   </div>
   <div class='panel-body'>
 <?php
-echo "<div class='lic'>";	
+echo "<div class='lic'>";
 echo "<div id ='editPlayer'><center><h1>".$username."</h1></center>";
 echo "<center><h4>UID: ".$player->uid."</h3></center>";
 echo "<center><h4>Player ID: ".$player->playerid."</h3></center>";
@@ -158,39 +134,39 @@ echo "<div class='panel panel-info'>
                 </tr>
               </thead>
               <tbody>
-			  
+
 <?php
 
 if (isset($_POST['donUpdate'])) {
-	
+
   $sql = "SELECT * FROM `players` WHERE `uid` = $uidPlayer";
   $result = mysqli_query($dbcon, $sql);
-  $player = $result->fetch_object();	
-	
+  $player = $result->fetch_object();
+
   if ($adminLev > 6) {
-	
+
     if ($_POST['donatorlvl'] != $player->donatorlvl) {
     $message = "Admin ".$user." has changed ".$player->name."(".$player->playerid.")"." donator level from ".$player->donatorlvl." to ".$_POST['donatorlvl'];
     $logQ = "INSERT INTO log (user,action,level) VALUES ('$user','$message',1)";
     mysqli_query($dbcon, $logQ);
   }
-	
+
   if ($_POST['blacklist'] != $player->blacklist) {
     $message = "Admin ".$user." has changed ".$player->name."(".$player->playerid.")"." blacklist status from ".$player->blacklist." to ".$_POST['blacklist'];
     $logQ = "INSERT INTO log (user,action,level) VALUES ('$user','$message',1)";
     mysqli_query($dbcon, $logQ);
   }
   $UpdateQ = "UPDATE players SET blacklist='$_POST[blacklist]', donatorlvl='$_POST[donatorlvl]' WHERE uid='$uidPlayer'";
-  mysqli_query($dbcon, $UpdateQ);	
-	
+  mysqli_query($dbcon, $UpdateQ);
+
   } elseif ($adminLev > 4) {
-		
+
   if ($_POST['donatorlvl'] != $player->donatorlvl) {
     $message = "Admin ".$user." tried to change ".$player->name."(".$player->playerid.")"." donator level from ".$player->donatorlvl." to ".$_POST['donatorlvl'];
     $logQ = "INSERT INTO log (user,action,level) VALUES ('$user','$message',1)";
     mysqli_query($dbcon, $logQ);
   }
-	
+
   if ($_POST['blacklist'] != $player->blacklist) {
     $message = "Admin ".$user." has changed ".$player->name."(".$player->playerid.")"." blacklist status from ".$player->blacklist." to ".$_POST['blacklist'];
     $logQ = "INSERT INTO log (user,action,level) VALUES ('$user','$message',1)";
@@ -199,24 +175,24 @@ if (isset($_POST['donUpdate'])) {
   $UpdateQ = "UPDATE players SET blacklist='$_POST[blacklist]' WHERE uid='$uidPlayer'";
   mysqli_query($dbcon, $UpdateQ);
   }else {
-		
+
   if ($_POST['donatorlvl'] != $player->donatorlvl) {
     $message = "Admin ".$user." tried to change ".$player->name."(".$player->playerid.")"." donator level from ".$player->donatorlvl." to ".$_POST['donatorlvl'];
     $logQ = "INSERT INTO log (user,action,level) VALUES ('$user','$message',1)";
     mysqli_query($dbcon, $logQ);
   }
-	
+
   if ($_POST['blacklist'] != $player->blacklist) {
     $message = "Admin ".$user." tried to change ".$player->name."(".$player->playerid.")"." blacklist status from ".$player->blacklist." to ".$_POST['blacklist'];
     $logQ = "INSERT INTO log (user,action,level) VALUES ('$user','$message',1)";
     mysqli_query($dbcon, $logQ);
   }
-		
+
   }
 }
 
 
-	
+
 $sqlget = "SELECT * FROM players WHERE uid=$uidPlayer;";
 $search_result = mysqli_query($dbcon, $sqlget) or die ('Connection could not be established');
 
@@ -227,14 +203,14 @@ echo "<td>"."<input class='form-control' type=text style = 'width: 100%;' name=d
 echo "<td>"."<input class='form-control' type=text style = 'width: 100%;' name=blacklist value=".$row['blacklist']." </td>";
 echo "<td>"."<input class='btn btn-primary btn-outline' type=submit name=donUpdate value=Update"." </td>";
 echo "</form>";
-		
+
   echo "</tr>";
 }
   echo "</table></div>";
-	
-	
 
-  
+
+
+
 
 echo "</div>";
 echo "</div>";
@@ -255,37 +231,33 @@ echo "<div class='panel panel-info'>
                 </tr>
               </thead>
               <tbody>
-			  
+
 <?php
-	
+
   $sqlget = "SELECT * FROM notes WHERE uid=$uidPlayer;";
 $search_result = mysqli_query($dbcon, $sqlget) or die ('Connection could not be established');
 
 while ($row = mysqli_fetch_array($search_result, MYSQLI_ASSOC)) {
   echo "<tr>";
-//echo "$row[note_text]";
 
   if ($row['warning'] == 2) {
-  //echo "<td style=background-color:#FFA500;>Strike</td>";
-  //echo "<td style=background-color:#FFA500;><input class='form-control' type=text name=warn value='$row[warning]' .</td>";
 
 echo "<td style=background-color:#FFA500;>".$row['staff_name']." </td>";
 echo "<td style=background-color:#FFA500;>".$row['note_text']." </td>";
 echo "<td style=background-color:#FFA500;>".$row['note_updated']." </td>";
 
-	
+
   }elseif ($row['warning'] == 3) {
-  //echo "<td style=background-color:#FF0000;><input class='form-control' type=text name=warn value='$row[warning]' .</td>";
-	
+
 echo "<td style=background-color:#FF0000;>".$row['staff_name']." </td>";
 echo "<td style=background-color:#FF0000;>".$row['note_text']." </td>";
 echo "<td style=background-color:#FF0000;>".$row['note_updated']." </td>";
   }else {
-		
+
 echo "<td>".$row['staff_name']." </td>";
 echo "<td>".$row['note_text']." </td>";
 echo "<td>".$row['note_updated']." </td>";
-		
+
   }
   echo "</tr>";
 }
@@ -312,17 +284,17 @@ echo "<div class='panel panel-info'>
   foreach ($return as $value) {
     if (strpos($value, "1") == TRUE) {
       $name = before(',', $value);
-      $display = explode("_", $name); 
-      $displayN = $display['2']; 
+      $display = explode("_", $name);
+      $displayN = $display['2'];
       echo "<button type='button' id=".$name." class='license btn btn-success btn-sm' onClick='post1(this.id);'>".$displayN."</button> ";
-		
+
     }else {
       $name = before(',', $value);
-      $display = explode("_", $name); 
+      $display = explode("_", $name);
       $displayN = $display['2'];
       echo "<button type='button' id=".$name." class='btn btn-secondary btn-sm' onClick='post(this.id);'>".$displayN."</button> ";
 
-		
+
     }
   }
 echo "  </div>
@@ -333,29 +305,29 @@ echo "<div id ='civlic1'>";
   if ($player->med_licenses !== '"[]"' && $player->med_licenses !== '') {
     $return = stripArray($player->med_licenses, 0);
     $return = str_replace('[[', "", $return);
-		
+
 echo "<div class='panel panel-info'>
   <div class='panel-heading'>
     <h3 class='panel-title'>Medic Licenses</h3>
   </div>
   <div class='panel-body'>";
-  
- 
+
+
   foreach ($return as $value) {
     if (strpos($value, "1") == TRUE) {
       $name = before(',', $value);
-      $display = explode("_", $name); 
+      $display = explode("_", $name);
       $displayN = $display['2'];
       echo "<button type='button' id=".$name." class='btn btn-success btn-sm' onClick='post1(this.id);'>".$displayN."</button> ";
-		
+
     }else {
       $name = before(',', $value);
       if ($name != "") {
-      $display = explode("_", $name); 
+      $display = explode("_", $name);
       $displayN = $display['2'];
       echo "<button type='button' id=".$name." class='btn btn-secondary btn-sm' onClick='post(this.id);'>".$displayN."</button> ";
       }
-		
+
     }
   }
   echo "  </div>
@@ -373,25 +345,25 @@ echo "<div id ='civlic2'>";
     <h3 class='panel-title'>Cop Licenses</h3>
   </div>
   <div class='panel-body'>";
-		
+
   foreach ($return as $value) {
     if (strpos($value, "1") == TRUE) {
     $name = before(',', $value);
-    $display = explode("_", $name); 
+    $display = explode("_", $name);
     $displayN = $display['2'];
     echo "<button type='button' id=".$name." class='btn btn-success btn-sm' onClick='post1(this.id);'>".$displayN."</button> ";
-		
+
     }else {
       $name = before(',', $value);
       if ($name != "") {
-      $display = explode("_", $name); 
+      $display = explode("_", $name);
       $displayN = $display['2'];
       echo "<button type='button' id=".$name." class='btn btn-secondary btn-sm' onClick='post(this.id);'>".$displayN."</button> ";
       }
-		
+
     }
-		
-	
+
+
   }
     echo "  </div>
 </div>";
@@ -407,10 +379,10 @@ echo "</div>";
 
 
 if (isset($_POST['remove'])) {
-	
+
 $licReset = str_replace("1", "0", $player->civ_licenses);
 $sql = "UPDATE `players` SET `civ_licenses`='$licReset' WHERE uid ='$uidPlayer'";
-$result = mysqli_query($dbcon, $sql); 
+$result = mysqli_query($dbcon, $sql);
 
     $message = "Admin ".$user." has removed all licenses from ".$player->name."(".$player->playerid.")";
     $logQ = "INSERT INTO log (user,action,level) VALUES ('$user','$message',1)";
@@ -418,10 +390,10 @@ $result = mysqli_query($dbcon, $sql);
 
 };
 if (isset($_POST['give'])) {
-	
+
 $licReset = str_replace("0", "1", $player->civ_licenses);
 $sql = "UPDATE `players` SET `civ_licenses`='$licReset' WHERE uid ='$uidPlayer'";
-$result = mysqli_query($dbcon, $sql); 
+$result = mysqli_query($dbcon, $sql);
 
     $message = "Admin ".$user." has added all licenses to ".$player->name."(".$player->playerid.")";
     $logQ = "INSERT INTO log (user,action,level) VALUES ('$user','$message',1)";
@@ -441,33 +413,27 @@ function post (id)
 {
 var newid = "#" + id;
 
-	//document.getElementById(id).style.color = "red";
-	//document.getElementById(id).style.backgroundColor = "#00FF00";
-	//window.location.reload(); 
-	//alert(newid);
 	$(newid).toggleClass("btn-secondary btn-success");
-	
+
 	$.post('changeLicense.php',{id:id,uid:<?php echo $uidPlayer?>},
 	function(data)
 	{
-		//$('h5').html(data);
-		
+
+
 	});
 }
 
 function post1 (id)
 {
 var newid = "#" + id;
-	//document.getElementById(id).style.color = "red";
-	//document.getElementById(id).style.backgroundColor = "#FF0000";
+
 	 $(newid).toggleClass("btn-secondary btn-success");
-	
+
 	var newid = id;
 	$.post('changeLicense.php',{id:id,uid:<?php echo $uidPlayer?>},
 	function(data)
 	{
-		//$('h5').html(data);
-		
+
 	});
 }
 
