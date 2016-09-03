b202a91050db0eed7cbaf69ffa1b718718acac79
Jason_000 <jasonhall96686@yahoo.com>
2016-09-03 21:31:37 +0100

    Removed ChatRoom

--- chatRoom/vendor/mpratt/relativetime/Tests/Autoload.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__.'/../Lib/RelativeTime/Autoload.php';
-date_default_timezone_set('UTC');
-?>
--- home.php
+++ home.php
@@ -4,36 +4,35 @@ session_start();
 ob_start();
 
 if (!isset($_SESSION['logged'])) {
-    header("Location: index.php");
+    header('Location: index.php');
 }
 
 $adminLev = $_SESSION['adminLevel'];
 $user = $_SESSION['user'];
 
-$conecG = "work";
+$conecG = 'work';
 $_SESSION['conecFail'] = $conecG;
 
 if ($adminLev < 1) {
-  header("Location: index.php");
+    header('Location: index.php');
 }
 
-include('verifyPanel.php');
+include 'verifyPanel.php';
 masterconnect();
 
 $players = 0;
 $money = 0;
 
-$sqlget = "SELECT * FROM players";
-$sqldata = mysqli_query($dbcon, $sqlget) or die ('Connection could not be established');
+$sqlget = 'SELECT * FROM players';
+$sqldata = mysqli_query($dbcon, $sqlget) or die('Connection could not be established');
 
 while ($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)) {
-
-  $players++;
-  $money = $money + $row['cash'] + $row['bankacc'];
+    ++$players;
+    $money = $money + $row['cash'] + $row['bankacc'];
 }
 
-$sqlgetVeh = "SELECT * FROM vehicles";
-$sqldataVeh = mysqli_query($dbcon, $sqlgetVeh) or die ('Connection could not be established');
+$sqlgetVeh = 'SELECT * FROM vehicles';
+$sqldataVeh = mysqli_query($dbcon, $sqlgetVeh) or die('Connection could not be established');
 
 $vehicles = mysqli_num_rows($sqldataVeh);
 ?>
@@ -75,126 +74,148 @@ $vehicles = mysqli_num_rows($sqldataVeh);
 
   <body>
 <?php
-include('header/header.php');
+include 'header/header.php';
 ?>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 style = "margin-top: 70px">Dashboard</h1>
 		  <p class="page-header">Dashboard of the panel.</p>
 <?php
     //Max players
+    echo '
+    <div class="row">
+    <div class="col-lg-5">
+    ';
 
-    echo	"<div id='rcorners1'>";
-    echo		"<div class='box-top'><center><h1>Players</h1></div>";
-    echo		"<div class='box-panel'><p></p>";
-    echo		"<p><br><center>There are currently ".$players." players signed up on the server.</p>";
-    echo		"</div>";
-    echo	"</div>";
+    echo    "<div id='rcorners1'>";
+    echo        "<div class='box-top'><center><h1>Players</h1></div>";
+    echo        "<div class='box-panel'><p></p>";
+    echo        '<p><br><center>There are currently '.$players.' players signed up on the server.</p>';
+    echo        '</div>';
+    echo    '</div>';
+
+    echo    '</div>';
+    echo '<div class="col-lg-5">';
 
     //Vehicles
 
-    echo	"<div id='rcorners2'>";
-    echo		"<div class='box-top'><center><h1>Vehicles</h1></div>";
-    echo		"<div class='box-panel'><p></p>";
-    echo		"<p><br><center>There are currently ".$vehicles." vehicles.</p>";
-    echo		"</div>";
-    echo	"</div>";
+    echo    "<div id='rcorners2'>";
+    echo        "<div class='box-top'><center><h1>Vehicles</h1></div>";
+    echo        "<div class='box-panel'><p></p>";
+    echo        '<p><br><center>There are currently '.$vehicles.' vehicles.</p>';
+    echo        '</div>';
+    echo    '</div>';
+
+    echo    '</div>';
+    echo '<div class="col-lg-1">';
 
     //?
-$money = "$".number_format($money, 2);
+$money = '$'.number_format($money, 2);
 
-    echo	"<div id='rcorners3'>";
-    echo		"<div class='box-top'><center><h1>Total Money</h1></div>";
-    echo		"<div class='box-panel'><p></p>";
-    echo		"<p><br><center>There is a total of ".$money." on the server.</p>";
-    echo		"</div>";
-    echo	"</div>";
+    echo    "<div id='rcorners3'>";
+    echo        "<div class='box-top'><center><h1>Total Money</h1></div>";
+    echo        "<div class='box-panel'><p></p>";
+    echo        '<p><br><center>There is a total of '.$money.' on the server.</p>';
+    echo        '</div>';
+    echo    '</div>';
 
+    echo    '</div>';
+    echo    '</div>';
+    echo    '<div class="row">';
+    echo '<div class="col-lg-5">';
 
-    echo	"<div id='rcorners4'>";
-    echo		"<div class='box-top'><center><h1>Global Message</h1></div>";
-    echo		"<div class='box-panel'><p></p>";
-    echo 			"<form action=home.php method=post>";
-    echo 		"<div class = 'textInput'><td>"."<center><input class='form-control' type=text name=global value='' < /td></div><br>";
-    echo 		"<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=send value=Send"." </td></div>";
-    echo		"</div>";
-    echo	"</div>";
-    echo  "</form>";
+    echo    "<div id='rcorners4'>";
+    echo        "<div class='box-top'><center><h1>Global Message</h1></div>";
+    echo        "<div class='box-panel'><p></p>";
+    echo            '<form action=home.php method=post>';
+    echo        "<div class = 'textInput'><td>"."<center><input class='form-control' type=text name=global value='' < /td></div><br>";
+    echo        "<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=send value=Send".' </td></div>';
+    echo        '</div>';
+    echo    '</div>';
+    echo  '</form>';
 
+    echo    '</div>';
+    echo '<div class="col-lg-5">';
 
-    echo	"<div id='rcorners5'>";
-    echo		"<div class='box-top'><center><h1>Restart Server</h1></div>";
-    echo		"<div class='box-panel'><p></p>";
-    echo 			"<form action=home.php method=post>";
-    echo 		"<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=restart value=Restart"." </td></div>";
-    echo		"</div>";
-    echo	"</div>";
-    echo  "</form>";
+    echo    "<div id='rcorners5'>";
+    echo        "<div class='box-top'><center><h1>Restart Server</h1></div>";
+    echo        "<div class='box-panel'><p></p>";
+    echo            '<form action=home.php method=post>';
+    echo        "<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=restart value=Restart".' </td></div>';
+    echo        '</div>';
+    echo    '</div>';
+    echo  '</form>';
 
+    echo    '</div>';
+    echo '<div class="col-lg-1">';
 
-    echo	"<div id='rcorners6'>";
-    echo		"<div class='box-top'><center><h1>Stop Server</h1></div>";
-    echo		"<div class='box-panel'><p></p>";
-    echo 			"<form action=home.php method=post>";
-    echo 		"<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=stop value=Stop"." </td></div>";
-    echo		"</div>";
-    echo	"</div>";
-    echo  "</form>";
+    echo    "<div id='rcorners6'>";
+    echo        "<div class='box-top'><center><h1>Stop Server</h1></div>";
+    echo        "<div class='box-panel'><p></p>";
+    echo            '<form action=home.php method=post>';
+    echo        "<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=stop value=Stop".' </td></div>';
+    echo        '</div>';
+    echo    '</div>';
+    echo  '</form>';
 
-    echo	"<div id='rcorners7'>";
-    echo		"<div class='box-top'><center><h1>Admin Chatroom</h1></div>";
-    echo		"<div class='box-panel'><p></p>";
-    echo 			"<form action=home.php method=post>";
-    echo 		"<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=chat value=Chatroom"." </td></div>";
-    echo		"</div>";
-    echo	"</div>";
-    echo  "</form>";
+    echo    '</div>';
+    echo    '</div>';
+    echo    '<div class="row">';
+    echo '<div class="col-lg-2">';
+    echo    '</div>';
+    echo '<div class="col-lg-5">';
 
-    echo	"<div id='rcorners8'>";
-    echo		"<div class='box-top'><center><h1>Help</h1></div>";
-    echo 		"<div class='box-top'><center><h4>For general help on the panel!</h4></div>";
-    echo		"<div class='box-panel'><p></p>";
-    echo 			"<form action=home.php method=post>";
-    echo 		"<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=help value=Help"." </td></div>";
-    echo		"</div>";
-    echo	"</div>";
-    echo  "</form>";
+    echo    "<div id='rcorners8'>";
+    echo        "<div class='box-top'><center><h1>Help</h1></div>";
+    echo        "<div class='box-top'><center><h4>For general help on the panel!</h4></div>";
+    echo        "<div class='box-panel'><p></p>";
+    echo            '<form action=home.php method=post>';
+    echo        "<div class = 'textSend'><td>"."<center><input class='btn btn-primary btn-outline' type=submit name=help value=Help".' </td></div>';
+    echo        '</div>';
+    echo    '</div>';
+    echo  '</form>';
 
+    echo    '</div>';
+    echo '<div class="col-lg-1">';
 
 if (isset($_POST['send'])) {
-  if ($adminLev > 6) {
-$send = $_POST['global'];
-$_SESSION['send'] = $send;
-header("Location: rCon/rcon-mess.php");
-    $message = "Admin ".$user." has sent a global message (".$send.")";
-    logIt($user, $message, $dbcon);
-  }else {header("Location: ../lvlError.php"); }
+    if ($adminLev > 6) {
+        $send = $_POST['global'];
+        $_SESSION['send'] = $send;
+        header('Location: rCon/rcon-mess.php');
+        $message = 'Admin '.$user.' has sent a global message ('.$send.')';
+        logIt($user, $message, $dbcon);
+    } else {
+        header('Location: ../lvlError.php');
+    }
 }
 
 if (isset($_POST['restart'])) {
-  if ($adminLev > 6) {
-header("Location: rCon/rcon-restart.php");
-  }else {header("Location: ../lvlError.php"); }
+    if ($adminLev > 6) {
+        header('Location: rCon/rcon-restart.php');
+    } else {
+        header('Location: ../lvlError.php');
+    }
 }
 
 if (isset($_POST['chat'])) {
-
-header("Location: chatRoom/chatRoom.php");
-
+    header('Location: chatRoom/chatRoom.php');
 }
 
 if (isset($_POST['stop'])) {
-  if ($adminLev > 6) {
-header("Location: rCon/rcon-stop.php");
-  }else {header("Location: ../lvlError.php"); }
+    if ($adminLev > 6) {
+        header('Location: rCon/rcon-stop.php');
+    } else {
+        header('Location: ../lvlError.php');
+    }
 }
 
 if (isset($_POST['help'])) {
-header("Location: help.php");
+    header('Location: help.php');
 }
 ob_end_flush();
 ?>
 <div class = "donate">
-<div class ="headD"><h3>Donate to see further updates!<h3></div>
+<div class ="headD"><center><h3>Donate to see further updates!<h3></center></div>
 	<div class ="donateB"><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 	<input type="hidden" name="cmd" value="_donations">
 	<input type="hidden" name="business" value="jasonhall96686@yahoo.com">
@@ -206,6 +227,9 @@ ob_end_flush();
 	<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
 	<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 	</form></div>
+</div>
+
+</div>
 </div>
           </div>
         </div>
