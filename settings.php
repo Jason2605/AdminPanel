dc9c167f5ae780cc0db348a92420aed42fba812c
Jason_000 <jasonhall96686@yahoo.com>
2016-10-26 15:43:31 +0100

    Fix CSS

--- settings.php
+++ settings.php
@@ -23,53 +23,27 @@ if ($staffPerms['superUser'] != '1') {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
-
     <title>Admin Panel - Settings</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="/dist/css/bootstrap.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="dist/css/bootstrap.css" rel="stylesheet">
     <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="/styles/dashboard.css" rel="stylesheet">
-	<link href="/styles/global.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <link href="styles/dashboard.css" rel="stylesheet">
+	<link href="styles/global.css" rel="stylesheet">
     <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
-
   <body>
-
-
-
-
-
 <?php
 include 'header/header.php';
 ?>
-
-
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 style = "margin-top: 70px">Settings Menu</h1>
-		  <p class="page-header">Settings menu of the panel, allows you to change panel settings.</p>
-
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+<h1 style = "margin-top: 70px">Settings Menu</h1>
+    <p class="page-header">Settings menu of the panel, allows you to change panel settings.</p>
 <div class='panel panel-info'>
-<div class='panel-heading'>
-<h3 class='panel-title'>Server Info</h3>
-</div>
+    <div class='panel-heading'>
+        <h3 class='panel-title'>Server Info</h3>
+    </div>
 <div class='panel-body'>
 
 <form action = "updateSettings.php" method="post">
