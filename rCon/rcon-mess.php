2ee567b5d46b2dfc03c2aa3134c92357b1daa438
Jason_000 <Jason2605@users.noreply.github.com>
2017-04-14 00:50:21 +0100

    Update rcon-mess.php

--- rCon/rcon-mess.php
+++ rCon/rcon-mess.php
@@ -22,10 +22,6 @@ $mess = $_SESSION['send'];
 if ($mess == '') {
     header('Location: ../home.php');
 } else {
-    echo $mess;
-    echo '<br>';
-
     $mess = $rcon->say_global($mess);
-
     header('Location: ../home.php');
 }
