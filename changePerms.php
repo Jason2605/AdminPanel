c3bdfb9440b5108891b98ece6f2a63bc67fd5091
Jason_000 <Jason2605@users.noreply.github.com>
2017-06-08 22:06:07 +0100

    Stop foregin post requests - Thanks Trahax/BigJ

--- changePerms.php
+++ changePerms.php
@@ -3,33 +3,37 @@
 session_start();
 ob_start();
 
+$staffPerms = $_SESSION['perms'];
+$user = $_SESSION['user'];
+
 if (!isset($_SESSION['logged'])) {
     header('Location: index.php');
 }
 
-$adminLev = $_SESSION['adminLevel'];
-$user = $_SESSION['user'];
+if ($staffPerms['superUser'] == '1') {
 
-include 'verifyPanel.php';
-masterconnect();
+    include 'verifyPanel.php';
+    masterconnect();
 
-$id = $_POST['id'];
-$staffId = $_POST['uid'];
+    $id = $_POST['id'];
+    $staffId = $_POST['uid'];
 
-$change = explode('_', $_POST['id']);
+    $change = explode('_', $_POST['id']);
 
-$sql = "SELECT * FROM `users` WHERE ID = $staffId";
-$result = mysqli_query($dbcon, $sql);
-$user = $result->fetch_object();
+    $sql = "SELECT * FROM `users` WHERE ID = $staffId";
+    $result = mysqli_query($dbcon, $sql);
+    $user = $result->fetch_object();
 
-$licences = $user->permissions;
-$num = strpos($licences, $change['0']) + strlen($change['0']) + 2;
+    $licences = $user->permissions;
+    $num = strpos($licences, $change['0']) + strlen($change['0']) + 2;
 
-if ($licences[$num] == 0) {
-    $licences[$num] = 1;
-} elseif ($licences[$num] == 1) {
-    $licences[$num] = 0;
+    if ($licences[$num] == 0) {
+        $licences[$num] = 1;
+    } elseif ($licences[$num] == 1) {
+        $licences[$num] = 0;
+    }
+
+    $sql = "UPDATE `users` SET `permissions` = '$licences' WHERE ID = $staffId";
+    $result = mysqli_query($dbcon, $sql);
+
 }
-
-$sql = "UPDATE `users` SET `permissions` = '$licences' WHERE ID = $staffId";
-$result = mysqli_query($dbcon, $sql);
